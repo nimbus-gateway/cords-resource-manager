e32f7b4aee602787a78f467dc5e1b6d203e8a6cf
tharindu <tharindu.prf@gmail.com>
2024-04-24 17:35:26 +0100

    Get Semantic Description Through The API

--- src/controllers/ml_model_controller.py
+++ src/controllers/ml_model_controller.py
@@ -1,16 +1,22 @@
 from flask import request, Response, json, Blueprint, jsonify, abort
-from src.models.ml_model import MlModel, MLModelSchema, ErrorReponseSchema, MLModelSchemaResponse
+from src.models.ml_model import MlModel, MLModelSchema, ErrorReponseSchema, MLModelSchemaResponse, MLSemanticSchema
 from apifairy import body, other_responses, response
-# from src import bcrypt, db
-# from datetime import datetime
-# import jwt
-# import os
+from cords_semantics.semantics import MlSemanticManager
+from cords_semantics.mlflow import convert_tags_to_dictionary, extract_mlflow_semantics
+import logging
+from config import settings
+import mlflow
+
+
+logging.basicConfig(level=logging.DEBUG)  
+
 
 # user controller blueprint to be registered with api blueprint
 ml_models = Blueprint("ml_models", __name__, template_folder='templates')
 ml_model_schema = MLModelSchema()
 ml_model_response_schema = MLModelSchemaResponse()
 error_response_schema = ErrorReponseSchema()
+ml_semantic_schema = MLSemanticSchema()
 new_model = MlModel()
 
 
@@ -30,19 +36,18 @@ def add_model(kwargs):
         name = data.get('name')
         version = data.get('version')
         description = data.get('description')
-        semantics = data.get('semantics')
+        ml_flow_model_path = data.get('ml_flow_model_path')
         
-        if not all([name, version, description, semantics]):
+        if not all([name, version, description, ml_flow_model_path]):
             error = {"status": "failed", "message": "Error Occured", "error": "Missing data"}
             return error, 400
 
-        model_id = new_model.add_model(name, version, description, semantics)
-        resp = kwargs
-        resp['model_id'] = str(model_id)
-
-
-        return resp, 201
-    
+        resp = new_model.add_model(name, version, description, ml_flow_model_path)
+        if resp:
+            return resp, 201
+        else:
+            error = {"status": "failed", "message": "Error Occured", "error": "Data Base Insert Failed"}
+            return error, 400
 
 
     except Exception as e:
@@ -58,8 +63,73 @@ def get_model(model_id):
 
     model_response = new_model.get_model(model_id)
 
-    if model_response == "Model not found.":
-        abort(404)
+    if model_response:
+        return model_response[0]
     else:
-        model_response['model_id'] = model_id
-        return model_response
+        abort(404)
+        
+
+
+
+@ml_models.route('/update_model/<string:model_id>', methods=['PUT'])
+@body(ml_model_schema)
+@response(ml_model_response_schema)
+@other_responses({404: 'Entry not found'})
+def update_model(kwargs, model_id):
+    """Update a ML Model Entry"""
+    try:
+        data = request.get_json()
+        if not data:
+            error = {"status": "failed", "message": "Error Occured", "error": "No data provided"}
+            return error, 400
+
+        name = data.get('name')
+        version = data.get('version')
+        description = data.get('description')
+        ml_flow_model_path = data.get('ml_flow_model_path')
+
+        
+        if not all([name, version, description, ml_flow_model_path]):
+            error = {"status": "failed", "message": "Error Occured", "error": "Missing data"}
+            return error, 400
+        #model_id = kwargs['model_id']
+        model_response = new_model.update_model(model_id, name, version, description, ml_flow_model_path)
+ 
+        if model_response:
+            return model_response[0]
+        else:
+            abort(404)
+    
+    except Exception as e:
+        error = {"status": "failed", "message": "Error Occured", "error":  str(e)}
+        return error, 500
+    
+
+@ml_models.route('/generate_semantics/<string:model_id>', methods = ["GET"])
+@response(ml_semantic_schema)
+def generate_semantics(model_id):
+    """Getting semantic description"""
+    try:
+        mlflow.set_tracking_uri(settings.MLFLOW_URI)
+        semantic_manager = MlSemanticManager('data/cordsml.rdf')
+        
+        ml_flow_run_id = new_model.get_mlflow_run_id(model_id)
+        logging.info("run id retrieved :%s", ml_flow_run_id)
+        mflow_tags = extract_mlflow_semantics(ml_flow_run_id)
+        logging.info("tags extracted from mlflow: %s", str(mflow_tags))
+
+        mlflow_semantics_dictionary = convert_tags_to_dictionary(mflow_tags)
+        logging.info("semantic in a dictionary: ", mlflow_semantics_dictionary)
+
+        semantic_graph = semantic_manager.create_model_semantics(mlflow_semantics_dictionary)
+        jsonld_output_string = semantic_graph.serialize(format='json-ld')
+
+        jsonld_output = json.loads(jsonld_output_string)
+
+        response = {"model_id": model_id, "semantics": jsonld_output}
+ 
+        return response
+    
+    except Exception as e:
+        error = {"status": "failed", "message": "Error Occured", "error":  str(e)}
+        return error, 500
--- src/models/ml_model.py
+++ src/models/ml_model.py
@@ -1,6 +1,11 @@
 
 from tinydb import TinyDB, Query
 from src import ma
+import re
+import hashlib
+import json
+from datetime import datetime
+from marshmallow import Schema, fields
 
 class ErrorReponseSchema(ma.Schema):
     """Schema defining the attributes when creating a new model entry."""
@@ -8,7 +13,10 @@ class ErrorReponseSchema(ma.Schema):
     message = ma.String(required=True)
     error = ma.String(required=True)
 
-
+class MLSemanticSchema(ma.Schema):
+    """Schema defining the sematnic of a registeed ML Model."""
+    model_id = ma.String(required=True)
+    semantics = ma.List(fields.Dict(), required=True)
 
 class MLModelSchemaResponse(ma.Schema):
     """Schema defining the attributes after creating new model entry."""
@@ -16,14 +24,15 @@ class MLModelSchemaResponse(ma.Schema):
     name = ma.String(required=True)
     version = ma.String(required=True)
     description = ma.String(required=True)
-    semantics = ma.String(required=True)
+    ml_flow_model_path = ma.String(required=True)
+    timestamp = ma.String(required=True)
 
 class MLModelSchema(ma.Schema):
     """Schema defining the attributes when creating a new model entry."""
     name = ma.String(required=True)
     version = ma.String(required=True)
     description = ma.String(required=True)
-    semantics = ma.String(required=True)
+    ml_flow_model_path = ma.String(required=True)
 
 class MlModel():
     def __init__(self, db_path='src/db/db.json'):
@@ -34,28 +43,56 @@ class MlModel():
         self.db = TinyDB(db_path)
         self.Model = Query()
 
- 
-
-    def add_model(self, name, version, description, semantics):
+    def add_model(self, name, version, description, ml_flow_model_path):
         """
         Add a new model to the database.
         :param name: Name of the model.
         :param version: Version of the model.
         :param description: A short description of the model.
-        :semantics description: Semantic description of the model.
+        :ml_flow_model_path description: MLFlow Model Path as recorded in Path variable of MLFlow
         """
-        return self.db.insert({'name': name, 'version': version, 'description': description, 'semantics': semantics})
+        document = {'name': name, 'version': version, 'description': description, 'ml_flow_model_path': ml_flow_model_path}
+        model_id = self._create_hashed_id(document)
+        document['model_id'] = model_id
+        document['timestamp'] = str(datetime.now().isoformat())
+
+        if self._validate_mlflow_input(document['ml_flow_model_path']):
+            if self.db.insert(document):
+                return document
+            else:
+                return False
+        else:
+            raise ValueError("Invalid ml_flow_model_path")
+    
+    def update_model(self, model_id, name, version, description, ml_flow_model_path):
+        """
+        Update a new model to the database.
+        :param model_id: Id of the model.
+        :param name: Name of the model.
+        :param version: Version of the model.
+        :param description: A short description of the model.
+        :ml_flow_model_path description: MLFlow Model Path as recorded in Path variable of MLFlow
+        """
+
+        Model = Query()
+    
+        if self.db.update({'name': name, 'version': version, 'description': description, 
+                           'ml_flow_model_path': ml_flow_model_path, 'timestamp': str(datetime.now().isoformat())}, Model.model_id ==model_id):
+            return self.get_model(model_id)
+        else:
+            return False
 
     def get_model(self, model_id):
         """
         Retrieve a model by its ID.
         :param model_id: The ID of the model to retrieve.
         """
-        model = self.db.get(doc_id=model_id)
+        Model = Query()
+        model = self.db.search(Model.model_id == model_id)
         if model:
             return model
         else:
-            return "Model not found."
+            return False
 
     def query_models(self, search_criteria):
         """
@@ -64,3 +101,49 @@ class MlModel():
         """
         results = self.db.search(self.Model.matches(search_criteria))
         return results if results else "No models found matching the criteria."
+    
+    def get_mlflow_run_id(self, model_id):
+        """
+        Get the MLFlow Run ID of an artifact corresponding to a registered Model.
+        :param model_id: The ID of the model.
+        """
+        model = self.get_model(model_id)[0]
+        
+        if model:
+            artifact_path = model['ml_flow_model_path']
+            run_id = artifact_path.split('/')[2]
+            return run_id
+        
+        else:
+            return False
+
+    def _create_hashed_id(self, document):
+        # Serialize the document data to JSON format
+        # Ensure consistent ordering by sorting keys
+        doc_string = json.dumps(document, sort_keys=True)
+        
+        # Get current timestamp as a string
+        timestamp = datetime.now().isoformat()
+        
+        # Create a hash object
+        hash_obj = hashlib.sha256()
+        
+        # Update the hash object with the document string and timestamp
+        hash_obj.update(doc_string.encode('utf-8'))
+        hash_obj.update(timestamp.encode('utf-8'))
+        
+        # Return the hexadecimal digest of the hash
+        return hash_obj.hexdigest()
+    
+    def _validate_mlflow_input(self, input_string):
+        # Define the regex pattern
+        pattern = r"^mlflow-artifacts:/\d+/[a-zA-Z0-9]+/artifacts/[a-zA-Z0-9_\-]+$"
+
+        # Compile the regex pattern for efficiency if used repeatedly
+        regex = re.compile(pattern)
+
+        # Use the fullmatch method to check if the entire string conforms to the pattern
+        if regex.fullmatch(input_string):
+            return True
+        else:
+            return False
